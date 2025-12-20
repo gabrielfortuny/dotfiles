863883d1ca5fc4db55cb9215fc23a46a691d6d51
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2025-12-20 00:18:41 -0500

    Update run_once_before_darwin-install-homebrew.sh
    Fix PATH issue

--- .chezmoiscripts/run_once_before_darwin-install-homebrew.sh
+++ .chezmoiscripts/run_once_before_darwin-install-homebrew.sh
@@ -14,3 +14,10 @@ if ! command -v brew >/dev/null 2>&1; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+# Ensure Homebrew is added to PATH before installing packages
+if [ -x /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -x /usr/local/bin/brew ]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
