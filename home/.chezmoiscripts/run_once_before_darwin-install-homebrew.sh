efea21e588ecd2fc5a36afb60a3720227459450a
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2026-01-22 02:24:35 -0500

    brew shellenv fix

--- home/.chezmoiscripts/run_once_before_darwin-install-homebrew.sh
+++ home/.chezmoiscripts/run_once_before_darwin-install-homebrew.sh
@@ -14,8 +14,4 @@ if ! command -v brew >/dev/null 2>&1; then
 fi
 
 # Ensure Homebrew is added to PATH before installing packages
-if [ -x /opt/homebrew/bin/brew ]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-elif [ -x /usr/local/bin/brew ]; then
-  eval "$(/usr/local/bin/brew shellenv)"
-fi
+eval "$(brew shellenv)"
